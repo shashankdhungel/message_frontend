388edf9ad57d00840d3365ddb6593c2ec8a34508
shashankdhungel <shashank.dhungel@gmail.com>
2024-06-24 15:33:54 +1200

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import TopBar from "./Components/TopBar";
+import {Route, Routes} from "react-router";
+import ChatRoom from "./Components/ChatRoom";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <TopBar/>
+      <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/chatroom" element ={<ChatRoom/>}/>
+
+      </Routes>
     </div>
   );
 }
---
+++ src/consistents.js
@@ -0,0 +1,1 @@
+export const BaseUrl = "https://message-rho-pearl.vercel.app/"
